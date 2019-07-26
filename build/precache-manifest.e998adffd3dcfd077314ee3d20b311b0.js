5f5e8135474edc9f0b12f1d1c288a18d27725d22
roopamgarg <roopamg777@gmail.com>
2019-07-26 12:43:41 +0530

    comiit

---
+++ build/precache-manifest.e998adffd3dcfd077314ee3d20b311b0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b4ee527f17b974797da8852d71565c8",
+    "url": "/portfolio-2019/index.html"
+  },
+  {
+    "revision": "214d683911664316b0b7",
+    "url": "/portfolio-2019/static/css/main.431d5658.chunk.css"
+  },
+  {
+    "revision": "03e8db402d977ade17bd",
+    "url": "/portfolio-2019/static/js/2.fc82c5bf.chunk.js"
+  },
+  {
+    "revision": "214d683911664316b0b7",
+    "url": "/portfolio-2019/static/js/main.47b07546.chunk.js"
+  },
+  {
+    "revision": "082e7cb3b386e751ce75",
+    "url": "/portfolio-2019/static/js/runtime~main.d36a5cc7.js"
+  },
+  {
+    "revision": "8566e2336952927984495865df90963c",
+    "url": "/portfolio-2019/static/media/GothamLight.8566e233.ttf"
+  },
+  {
+    "revision": "73bb48c6e2cc710c570e80569afc212e",
+    "url": "/portfolio-2019/static/media/download.73bb48c6.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio-2019/static/media/logo.5d5d9eef.svg"
+  }
+]);
