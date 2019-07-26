8c487536aabf5fb07ee916d5ec66a65c6687006e
roopamgarg <roopamg777@gmail.com>
2019-07-26 11:49:49 +0530

    changes

---
+++ build/precache-manifest.60b9fdfe87085dd53fa3cb230dd87352.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13e4b635202b39f54baa2c967a8c0b7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d27f097bd3db49343a1",
+    "url": "/static/css/main.642fc9a8.chunk.css"
+  },
+  {
+    "revision": "03e8db402d977ade17bd",
+    "url": "/static/js/2.fc82c5bf.chunk.js"
+  },
+  {
+    "revision": "3d27f097bd3db49343a1",
+    "url": "/static/js/main.2953718b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8566e2336952927984495865df90963c",
+    "url": "/static/media/GothamLight.8566e233.ttf"
+  },
+  {
+    "revision": "73bb48c6e2cc710c570e80569afc212e",
+    "url": "/static/media/download.73bb48c6.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
